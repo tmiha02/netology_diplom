bf8a1039634d412fe858c535940ae821e14b7eea
Michael Tretyakov <64438245+tmiha02@users.noreply.github.com>
2023-07-08 00:17:29 +0700

    Update db.py

--- db.py
+++ db.py
@@ -9,14 +9,14 @@ class Database:
         self.create_table()
 
     def create_table(self):
-        self.cursor.execute(
-            'CREATE TABLE IF NOT EXISTS matches (user_id INTEGER, match_id INTEGER, PRIMARY KEY(user_id, match_id))')
+        self.cursor.execute('CREATE TABLE IF NOT EXISTS matches (user_id INTEGER PRIMARY KEY)')
 
-    def add_match(self, user_id, match_id):
-        self.cursor.execute('INSERT INTO matches (user_id, match_id) VALUES (?, ?)', (user_id, match_id))
+    def add_match(self, user_id):
+        self.cursor.execute('INSERT OR IGNORE INTO matches (user_id) VALUES (?)', (user_id,))
         self.conn.commit()
 
     def get_matches(self, user_id) -> List[int]:
-        self.cursor.execute('SELECT match_id FROM matches WHERE user_id=?', (user_id,))
+        self.cursor.execute('SELECT user_id FROM matches')
         return [match[0] for match in self.cursor.fetchall()]
+
 
